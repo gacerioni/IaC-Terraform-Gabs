0f53149847e599de4d4602530e72cc02ddf9c755
gacerioni <gacerioni@gmail.com>
2021-07-20 10:53:43 -0300

    v2

--- main.tf
+++ main.tf
@@ -42,6 +42,19 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = "ec2-gabs-tf-labs-sg"
+  description = "Security group for example usage with EC2 instance"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+}
+
 module "ec2" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
 
@@ -49,10 +62,25 @@ module "ec2" {
   instance_count         = 1
 
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
-  key_name               = "gabs_keypair_sa_east_1"
-  vpc_security_group_ids = ["sg-08380ffaa5110ce89"]
-  subnet_id              = "subnet-153d9b4e"
+  instance_type          = var.ec2-size
+  key_name               = var.key-pair
+  vpc_security_group_ids = [module.security_group.security_group_id]
+  subnet_id              = tolist(data.aws_subnet_ids.all.ids)[0]
+
+  associate_public_ip_address = true
+
+  user_data_base64 = base64encode(local.user_data)
+
+  enable_volume_tags = false
+  root_block_device = [
+    {
+      volume_type = "gp2"
+      volume_size = 10
+      tags = {
+        Name = "my-root-block"
+      }
+    },
+  ]
 
   tags = {
     Terraform   = "true"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "ec2-size" {
   type = string
   description = "The size of your EC2 instance, like t3a.micro."
 }
+
+variable "key-pair" {
+    type = string
+    description = "The Key-Pair name to SSH into this EC2 Instance."
+    default = "gabs_keypair_sa_east_1"
+}
