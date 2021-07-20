3e2925fb2473b01311a4d702fac44fba5a74b558
gacerioni <gacerioni@gmail.com>
2021-07-20 10:32:42 -0300

    3 try

--- main.tf
+++ main.tf
@@ -42,24 +42,10 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 4.0"
-
-  name        = "gabs-terraform-labs-sg"
-  description = "Security group for example usage with EC2 instance"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
-}
-
 resource "aws_eip" "this" {
   vpc      = true
   instance = module.ec2.id[0]
 }
-
 
 resource "aws_kms_key" "this" {
 }
@@ -70,45 +56,12 @@ resource "aws_network_interface" "this" {
   subnet_id = tolist(data.aws_subnet_ids.all.ids)[count.index]
 }
 
-module "ec2" {
-  source = "../../"
-
-  instance_count = 1
-
-  name          = "gabs-terraform-labs-ec2"
+resource "aws_instance" "ec2" {
   ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  #instance_type = var.ec2-size
+  instance_type = var.ec2-size
   subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
-  #  private_ips                 = ["172.31.32.5", "172.31.46.20"]
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-
-  user_data_base64 = base64encode(local.user_data)
-
-  enable_volume_tags = false
-  root_block_device = [
-    {
-      volume_type = "gp2"
-      volume_size = 10
-      tags = {
-        Name = "my-root-block"
-      }
-    },
-  ]
-
-  ebs_block_device = [
-    {
-      device_name = "/dev/sdf"
-      volume_type = "gp2"
-      volume_size = 5
-      encrypted   = true
-      kms_key_id  = aws_kms_key.this.arn
-    }
-  ]
 
   tags = {
-    "Env"      = "Private"
-    "Location" = "Secret"
+    Name = "HelloWorld"
   }
 }
