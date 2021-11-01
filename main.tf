c49cd57ea478a531fdba32e9c7a1acda1e4e6288
Gabriel Cerioni <gacerioni@gmail.com>
2021-11-01 20:36:05 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,6 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-  }
-}
 provider "kubernetes" {
 }
+
 resource "kubernetes_namespace" "test" {
   metadata {
     name = "bart-ns"
