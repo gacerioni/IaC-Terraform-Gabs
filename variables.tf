1c6a30f4219521b57c2123e4400809b13978db76
Gabriel Cerioni <gacerioni@gmail.com>
2021-11-01 19:24:49 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,4 @@
-variable "ec2-size" {
+variable "kubernetes-namespace" {
   type = string
-  description = "The size of your EC2 instance, like t3a.micro."
-}
-
-variable "key-pair" {
-    type = string
-    description = "The Key-Pair name to SSH into this EC2 Instance."
-    default = "gabs_keypair_sa_east_1"
+  description = "The Namespace that you want to create."
 }
