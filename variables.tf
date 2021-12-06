551c30ea7842ca97434587203f8e03879cd3ae5f
Gabriel Cerioni <gacerioni@gmail.com>
2021-12-06 13:30:57 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,43 @@
-variable "ec2-size" {
-  type = string
-  description = "The size of your EC2 instance, like t3a.micro."
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+    default     = ""
 }
-
-variable "key-pair" {
-    type = string
-    description = "The Key-Pair name to SSH into this EC2 Instance."
-    default = "gabs_keypair_sa_east_1"
+variable "acl" {
+    type        = string
+    description = "(Optional) The canned ACL to apply. Defaults to private. Conflicts with grant."
+    default     = "private"
+}
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "target_bucket" {
+    type        = string
+    description = "(Required) The name of the bucket that will receive the log objects."
+    default     = ""
+}
+variable "target_prefix" {
+    type        = string
+    description = "(Optional) To specify a key prefix for log objects."
+    default     = "log/"
+}
+variable "kms_master_key_id" {
+    type        = string
+    description = "(optional) The AWS KMS master key ID used for the SSE-KMS encryption. This can only be used when you set the value of sse_algorithm as aws:kms. The default aws/s3 AWS KMS master key is used if this element is absent while the sse_algorithm is aws:kms."
+    default     = "aws/s3"
+}
+variable "sse_algorithm" {
+    type        = string
+    description = "(required) The server-side encryption algorithm to use. Valid values are AES256 and aws:kms"
+    default     = "aws:kms"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "prod"
+        terraform   = "true"
+    }
 }
