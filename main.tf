09484bde75faf7b5bdca6e7227c55787d0ca9200
Gabriel Cerioni <gacerioni@gmail.com>
2022-02-14 20:01:52 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket_prefix = var.bucket_prefix
-  acl    = var.acl
+  tags = var.tags
+}
 
-tags = var.tags
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = var.acl
 }
