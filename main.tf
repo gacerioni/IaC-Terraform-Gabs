eb7a5552c4ea9ee4204ae6400db4ff6ec8622398
Gabriel Cerioni <gacerioni@gmail.com>
2022-04-27 09:46:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ resource "aws_s3_bucket_acl" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
   acl    = var.acl
 }
+
+resource "aws_instance" "harness-server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
