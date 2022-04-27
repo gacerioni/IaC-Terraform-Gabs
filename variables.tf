af3a1b578ce896b58228db03cb1f6172af2da538
Gabriel Cerioni <gacerioni@gmail.com>
2022-04-27 11:24:43 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,9 @@ variable "key-pair" {
     description = "The Key-Pair name to SSH into this EC2 Instance."
     default = "gabs_keypair_sa_east_1"
 }
+
+variable "iamrole" {
+    type = string
+    description = "The AMI Role to put as Instance Profile for your EC2."
+    default = "Ec2FullAccessRole"
+}
