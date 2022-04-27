b400fab23e75dcff0c85f35370f38fe43b393cf5
Gabriel Cerioni <gacerioni@gmail.com>
2022-04-27 10:56:30 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 output "ids" {
   description = "List of IDs of instances"
-  value       = module.ec2.id
+  value       = module.ec2_instance.id
 }
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances"
-  value       = module.ec2.public_dns
+  value       = module.ec2_instance.public_dns
 }
 
 output "vpc_security_group_ids" {
   description = "List of VPC security group ids assigned to the instances"
-  value       = module.ec2.vpc_security_group_ids
+  value       = module.ec2_instance.vpc_security_group_ids
 }
 
 output "tags" {
   description = "List of tags"
-  value       = module.ec2.tags
+  value       = module.ec2_instance.tags
 }
 
 
 output "instance_id" {
   description = "EC2 instance ID"
-  value       = module.ec2.id[0]
+  value       = module.ec2_instance.id[0]
 }
 
 output "instance_public_dns" {
   description = "Public DNS name assigned to the EC2 instance"
-  value       = module.ec2.public_dns[0]
+  value       = module.ec2_instance.public_dns[0]
 }
