48f63a65b35c5ba5baaecc28e4d4089962577e52
Gabriel Cerioni <gacerioni@gmail.com>
2022-04-27 10:20:39 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,19 @@ provider "aws" {
   region = "sa-east-1"
 }
 
+
+data "aws_ami" "amazon-2" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+  owners = ["amazon"]
+}
+
 resource "aws_instance" "harness-server" {
-  ami           = "ami-830c94e3"
+  ami           = data.aws_ami.amazon-2.id
   instance_type = var.instancetype
 
   tags = var.tags
