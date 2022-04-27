11c206d36c6afb27623c570305282df27741f98a
Gabriel Cerioni <gacerioni@gmail.com>
2022-04-27 10:00:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,9 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket_prefix = var.bucket_prefix
-  tags = var.tags
-}
-
-resource "aws_s3_bucket_acl" "s3_bucket" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  acl    = var.acl
-}
-
 resource "aws_instance" "harness-server" {
   ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  instance_type = var.instancetype
 
-  tags = {
-    Name = var.instance_name
-  }
+  tags = var.tags
+}
