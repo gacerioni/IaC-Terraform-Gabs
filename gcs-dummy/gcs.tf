5765235e184aa54119d7118e2ad9cd0889db916b
Gabriel Cerioni <gacerioni@gmail.com>
2022-12-30 15:03:48 -0300

    Update gcs.tf

--- gcs-dummy/gcs.tf
+++ gcs-dummy/gcs.tf
@@ -1,18 +1,5 @@
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
+resource "google_storage_bucket" "default-gabs-gcs" {
+  name = var.bucket_name
+  storage_class = var.storage_class
+  location = var.bucket_location
 }
