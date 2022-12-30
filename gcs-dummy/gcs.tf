0ee2ab4b37c30fb9b6e8c502c3c21ae7b05ee38e
Gabriel Cerioni <gacerioni@gmail.com>
2022-12-30 15:07:29 -0300

    Update gcs.tf

--- gcs-dummy/gcs.tf
+++ gcs-dummy/gcs.tf
@@ -1,3 +1,8 @@
+provider "google" {
+  project = var.project_id
+  region  = var.bucket_location
+}
+
 resource "google_storage_bucket" "default-gabs-gcs" {
   name = var.bucket_name
   storage_class = var.storage_class
