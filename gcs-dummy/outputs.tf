f61fa6bfd05a852f3daf978512bf88c592e40c23
Gabriel Cerioni <gacerioni@gmail.com>
2022-12-30 15:06:01 -0300

    Create outputs.tf

---
+++ gcs-dummy/outputs.tf
@@ -0,0 +1,14 @@
+output "region" {
+  value       = var.bucket_location
+  description = "GCloud Region"
+}
+
+output "project_id" {
+  value       = var.project_id
+  description = "GCloud Project ID"
+}
+
+output "bucket" {
+  value       = var.bucket_name
+  description = "GCloud Project ID"
+}
