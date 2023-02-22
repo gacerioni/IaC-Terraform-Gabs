052c0c487a4daabe58d5fbb33229844098af0533
Gabriel Cerioni <gacerioni@gmail.com>
2023-02-22 15:54:40 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,21 @@
+variable "region" {
+    type        = string
+    description = "AWS Region to create the bucket."
+    default     = "sa-east-1"
+}
+
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
     default     = ""
 }
+
 variable "acl" {
     type        = string
     description = "(Optional) The canned ACL to apply. Defaults to private. Conflicts with grant."
     default     = "private"
 }
+
 variable "tags" {
     type        = map
     description = "(Optional) A mapping of tags to assign to the bucket."
